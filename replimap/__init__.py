022002c4bfc7ad19dacde1809088a63ea9b7dd47
davidlu1001 <davidlu1001@gmail.com>
2025-12-21 13:18:11 +1300

    Bump version

--- replimap/__init__.py
+++ replimap/__init__.py
@@ -5,5 +5,5 @@ Point at your Production AWS environment and generate a cost-optimized,
 safe Staging environment in minutes.
 """
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
 __author__ = "RepliMap Team"
